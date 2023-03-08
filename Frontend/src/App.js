01b9617d8f6f0acb480d1625234144f55565b898
Lukas Lee <jiawei3e1@gmail.com>
2023-03-08 20:15:53 +0800

    Revert "Revert "Merge branch 'junbin' into staging""
    This reverts commit 77a8a0bbd63f64bb31d5814c611de8295cf46acb.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,22 @@
 import Layout from "./components/Layout/Layout";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+
+// importing scene/pages to set up routers
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,13 +26,29 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app">
-          <main className="content"></main>
-            <Topbar/>
+        <div className="app" style={{display: 'flex'}}> 
+          <Sidebar  style={{height: "100vh"}}/>
+            <main className="content" style={{flex: '1 auto'}}>
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                {/* <Route path="/team" element={<Team />} /> */}
+                {/* <Route path="/contacts" element={<Contacts />} /> */}
+                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                {/* <Route path="/form" element={<Form />} /> */}
+                {/* <Route path="/bar" element={<Bar />} /> */}
+                {/* <Route path="/pie" element={<Pie />} /> */}
+                {/* <Route path="/line" element={<Line />} /> */}
+                {/* <Route path="/faq" element={<FAQ />} /> */}
+                {/* <Route path="/geography" element={<Geography />} /> */}
+                {/* <Route path="/calendar" element={<Calendar />} /> */}
+              </Routes>
+            </main>
+
         </div>
       </ThemeProvider>
       {/* Once you comment out Layout below, the topbar that Jun Bin created would work. Once Layout is uncommented, it would not work. */}
-      <Layout /> 
+      {/* <Layout />  */}
     </ColorModeContext.Provider>
   );
 }
