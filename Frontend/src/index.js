77a8a0bbd63f64bb31d5814c611de8295cf46acb
Lukas Lee <jiawei3e1@gmail.com>
2023-03-08 20:10:58 +0800

    Revert "Merge branch 'junbin' into staging"
    This reverts commit fb90ac43766b5a7dc81ff7d9a8a29947e418c9b3, reversing
    changes made to 9f284722b9c497b14333248585cbd558539e5e34.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -7,16 +6,11 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "remixicon/fonts/remixicon.css";
 
-
-
-
 ReactDOM.render(
   <React.StrictMode>
-
-      <Router>
-        <App />
-      </Router>
-
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- Frontend/src/scenes/dashboard/index.jsx
+++ Frontend/src/scenes/dashboard/index.jsx
@@ -1,14 +1,5 @@
-import { Box } from "@mui/system";
-import Header from "../../components/AdminHeader";
-
 const Dashboard = () => {
-  return (
-    <Box m="20px">
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to Dashboard" />
-      </Box>
-    </Box>
-  );
-};
+    return <div>Dashboard</div>
+}
 
 export default Dashboard;
