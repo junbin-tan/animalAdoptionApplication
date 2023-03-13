7afd6259dab09ef0931291fce99a5b6e68bc2c1f
Lukas Lee <jiawei3e1@gmail.com>
2023-03-13 17:12:43 +0800

    added a way to retrieve Actual Member object to frontend
    refer to line 14 to line 30 of index.js to retrieve Latest actual current member data like donations, events etc.

--- public_frontend/src/helpers/Api.js
+++ public_frontend/src/helpers/Api.js
@@ -46,11 +46,9 @@ const Api = {
          body: JSON.stringify(data),
       });
     },
-    testAuthorization() { // testing method to ensure only authenticated user can call certain API methods, like we don't want unauthenticated user able to call API methods like createEvent
-      return fetch(`${SERVER_PREFIX}/member/testAuth`, {
+   getMember() { // get Actual member json object with all data like donations, testimonials, events, etc
+      return fetch(`${SERVER_PREFIX}/member/getMember/${Auth.getUser().email}`, {
          headers: {
-             Accept: "application/json",
-             "Content-Type": "application/json",
              "Authorization": `Bearer ${Auth.getAccessToken()}`,
          },
          method: "GET",
