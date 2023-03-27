ad7b89b076df66b12474e3d020558f9103e39590
hengyjj <hengyjj@hotmail.com>
2023-03-27 11:24:30 +0800

    updated testimonial date

--- public_frontend/src/pages/Testimonial/testimonialPage.jsx
+++ public_frontend/src/pages/Testimonial/testimonialPage.jsx
@@ -4,6 +4,7 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 //import { ProductService } from './service/ProductService';
 import Api from "../../helpers/Api";
+import moment from 'moment-timezone';
 
 export default function TestimonialPage() {
     const [testimonial, setTestimonial] = useState([]);
@@ -13,6 +14,7 @@ export default function TestimonialPage() {
             .then(data => setTestimonial(data));
     }, []);
 
+    
     /*
     useEffect( () => {
         const fetchData = async() => {
@@ -31,7 +33,16 @@ export default function TestimonialPage() {
         <h2 style={{textAlign: "center"}}> Testimonials</h2>
         <div className="card">
             <DataTable value={testimonial} showGridlines tableStyle={{ minWidth: '50rem' }}>
-                <Column field="date" header="Date Published"></Column>
+                <Column
+                field="date"
+                header="Date Published"
+                body={(rowData) => (
+                    <span>
+                    {moment(rowData.date, 'YYYY-MM-DDTHH:mm:ss.SSSZ').tz('Asia/Shanghai').format('MMMM Do YYYY, h:mm a')}
+                    </span>
+                )}
+                style={{ width: "20rem" }}
+                />
                 <Column field="message" header="Testimonial"></Column>
             </DataTable>
         </div>
